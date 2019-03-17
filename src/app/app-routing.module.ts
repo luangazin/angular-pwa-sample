542e474179d9220d130505bb2f002bb6a9d325af
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2019-03-16 23:36:11 -0300

    angular-material added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'nav', component: NavComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
